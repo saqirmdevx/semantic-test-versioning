47347010b6ae4e0388f68fb2d2fc9a57dced60d3
Naščák Martin <saqirm@gmail.com>
2023-08-14 15:55:47 +0200

    feat: Update version to v1.4

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,15 +5,6 @@ export default function Page() {
     <>
       <Header text='Web' />
       <p>Web page - Relevant change - Random change</p>
-      <p> Random change </p>
-      <p>Web page - Relevant change - Random change</p>
-      <p> Random change </p>
-      <p>Web page - Relevant change - Random change</p>
-      <p> Random change </p>
-      <p>Web page - Relevant change - Random change</p>
-      <p> Random change </p>
-      <p>Web page - Relevant change - Random change</p>
-      <p> Random change </p>
       <Button />
     </>
   );
