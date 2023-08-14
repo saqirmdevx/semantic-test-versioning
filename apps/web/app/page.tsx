dd888263d7f4f52bf13d2fe1b096fd334f2b0e77
Naščák Martin <saqirm@gmail.com>
2023-08-14 15:57:55 +0200

    feat: Update version

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,10 @@ export default function Page() {
     <>
       <Header text='Web' />
       <p>Web page - Relevant change - Random change</p>
+      <p>Web page - Relevant change - Random change</p>
+      <p>Web page - Relevant change - Random change</p>
+      <p>Web page - Relevant change - Random change</p>
+      <p>Web page - Relevant change - Random change</p>
       <Button />
     </>
   );
